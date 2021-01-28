d44c624629697dcb46dd1d57f6844f62dd8bdb6b
Nath81210 <78166954+Nath81210@users.noreply.github.com>
2021-01-28 18:10:45 +0100

    Add files via upload

---
+++ IPONMAP.bat
@@ -0,0 +1,19 @@
+@echo off
+color 9
+echo !!-CE SCRIPT INSTALERA TOUTE LES COMPOSANTES DE IPONMAP-!!
+echo NATHX#9090
+echo iponmap Usage: iponmap -t [ip...] >> Comande.txt 
+start cmd.exe 
+pause
+npm install -g iponmap
+
+
+
+
+
+
+
+
+
+
+
