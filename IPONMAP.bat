bd8b6964c1413c03918181ecf48f48f7badf79f7
Nath81210 <78166954+Nath81210@users.noreply.github.com>
2021-01-28 20:00:52 +0100

    Update IPONMAP.bat

--- IPONMAP.bat
+++ IPONMAP.bat
@@ -3,7 +3,6 @@ color 9
 echo !!-CE SCRIPT INSTALERA TOUTE LES COMPOSANTES DE IPONMAP-!!
 echo NATHX#9090
 echo iponmap Usage: iponmap -t [ip...] >> Comande.txt 
-start cmd.exe 
 pause
 npm install -g iponmap
 
